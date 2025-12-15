601d74b38fa70d52a3408a0e0c5ca9c4d1cf44bc
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2025-12-15 14:09:05 +0100

    Dodano podstawy projektu, klasę Wpis, .gitignore

---
+++ Projekt/PochodneKlasyWpis.cs
@@ -0,0 +1,61 @@
+﻿using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+public partial class Program
+{
+    // Interfejs klasy Wpis
+    public interface IWpis
+    {
+        // Funkcja wypisująca informacje o danym Wpisie (Notatce lub Zadaniu).
+        // Treść funkcji i wyniku zależy od klas pochodnych.
+        protected abstract string WypiszInformacje();
+    }
+
+    // Klasa abstrakcyjna Wpis - służy tylko do bycia bazą pod inne klasy
+    protected abstract class Wpis : IWpis
+    {
+        // Pola klasy Wpis
+        // id danego wpisu (generowane przy konstrukcji przez klasy pochodne Notatka i Zadanie, zapisywane w menedżerach)
+        private int id;
+        private string tytul;  // tytuł danego wpisu
+        private string tresc;  // treść danego wpisu
+        private DateTime dataUtworzenia;  // data utworzenia wpisu
+        private DateTime dataModyfikacji;  // data ostatniej edycji wpisu
+        // UWAGA - klasa 'Tag' jeszcze nie istnieje!
+        //private List<Tag> tagi;  // lista tagów przypisanych do danego wpisu
+
+
+        // Konstruktor
+        protected Wpis(string tytul, string tresc) : base()
+        {
+            this.tytul = tytul;  // Podany tytuł
+            this.tresc = tresc;  // Podana treść
+            dataUtworzenia = DateTime.Now;  // Czas utworzenia
+        }
+
+        // Zmienia dane wpisu na te podane w parametrach metody
+        protected void Edytuj(string tytul, string tresc)
+        {
+            this.tytul = tytul;
+            this.tresc = tresc;
+        }
+
+        // UWAGA - klasa 'Tag' jeszcze nie istnieje!
+        // Dodaje podany jako parametr 'tag' do Listy tagów 'tagi'
+        //protected void DodajTag(Tag tag)
+        //{
+        //    tagi.Add(tag);
+        //}
+
+        // UWAGA - klasa 'Tag' jeszcze nie istnieje!
+        // Usuwa podany jako parametr 'tag' z Listy tagów 'tagi'
+        //protected void UsunTag(Tag tag)
+        //{
+        //    tagi.Remove(tag);
+        //}
+
+        // Abstrakcyjna metoda pochodząca od interfejsu IWpis;
+        // Nic nie zwraca, metoda powinna zostać nadpisana przez klasy pochodne.
+        public abstract string WypiszInformacje();
+    }
+}
---
+++ Projekt/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Program główny
+// "partial class" oznacza jedynie, że kod całego programu jest porozsiewany po wielu plikach.
+// Wszystkie pliki w projekcie powinny zaczynać się od "public partial class Program"
+public partial class Program
+{
+
+    // Funkcja Main - wykonuje program
+    static void Main(string[] args)
+    {
+
+        // Menu główne
+        while (true)
+        {
+            Console.WriteLine("\n\nWybierz zadanie:\n1: (zadanie 1, zestaw d; podpunkty II i V)\n2: (zadanie 3)\n3: (zadanie 5)");
+            string command = Console.ReadLine();  // Odczytuje komendę z klawiatury - może to być liczba albo słowo.
+
+            switch (command)
+            {
+                // Przykładowa komenda 1
+                case "komenda1":
+                    //tu_idzie_nazwa_funkcji();  // przykładowe wyowłanie funkcji
+                    Console.WriteLine("Wywołano komendę1");
+                    break;
+
+                // Przykładowa komenda 2
+                case "komenda2":
+                    //tu_idzie_nazwa_funkcji();  // przykładowe wyowłanie funkcji
+                    Console.WriteLine("Wywołano komendę2");
+                    break;
+
+                // W przypadku podania nieodpowiedniej komendy, należy wpisać coś innego.
+                default:
+                    Console.WriteLine("Podaj poprawną komendę");
+                    break;
+            }
+        }
+    }
+}
