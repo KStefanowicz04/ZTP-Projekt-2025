e94954b655ab32ad89216975e3ffe997e9cb5fa4
Lukasz97947 <91849@student.pb.edu.pl>
2026-01-06 15:06:57 +0100

    Dodanie klasy fabrykazadan i male poprawki w kodzie

--- Projekt/Zadanie-Manager.cs
+++ Projekt/Zadanie-Manager.cs
@@ -6,13 +6,6 @@ using System.Runtime.CompilerServices;
 public partial class Program
 {
 
-    public enum StanZadania
-    {
-        Aktywne,
-        Wykonane,
-        Zalegle
-    }
-
     public enum Priorytet
     {
         Niski,
@@ -25,14 +18,15 @@ public partial class Program
         private Priorytet priorytet;
         private DateTime termin;
 
-        public Zadanie(string tytul,string tresc,Priorytet priorytet,DateTime termin) 
-            : base(tytul, tresc)
+        public Zadanie(StanZadania stan, Priorytet priorytet, DateTime termin
+            ) : base(tytul, tresc)
         {
+            this.stan = stan;
             this.priorytet = priorytet;
             this.termin = termin;
-            this.stan = StanZadania.Aktywne;
         }
-        public void Edytuj(string tytul,string tresc,Priorytet priorytet,DateTime termin)
+
+        public override void Edytuj(string tytul, string tresc, Priorytet priorytet, DateTime termin)
         {
             base.Edytuj(tytul, tresc);
             this.priorytet = priorytet;
@@ -46,6 +40,20 @@ public partial class Program
         {
             this.stan = stan;
         }
+        public void StanWykonane()
+        {
+            stan = StanZadania.Wykonane;
+        }
+
+        public void StanAktywne()
+        {
+            stan = StanZadania.Aktywne;
+        }
+
+        public void StanZalegle()
+        {
+            stan = StanZadania.Zalegle;
+        }
         public bool SprawdzCzyZalegle()
         {
             if (stan == StanZadania.Wykonane)
@@ -53,5 +61,102 @@ public partial class Program
 
             return DateTime.Now > termin;
         }
+        public override string WypiszInformacje()
+        {
+            return $"[ZADANIE] {tytu³} | {priorytet} | Termin: {termin:d} | Stan: {stan.GetType().Name}";
+        }
     }
-}
+    public class MenedzerZadan
+    {
+
+        private static MenedzerZadan instancja;
+        private FabrykaZadan fabryka;
+        private List<Zadanie> zadania;
+       
+        private MenedzerZadan()
+        {
+            fabryka = new FabrykaZadan();
+            zadania = new List<Zadanie>();
+        }
+        public static MenedzerZadan GetterInstancji()
+        {
+            if (instancja == null)
+            {
+                instancja = new MenedzerZadan();
+            }
+            return instancja;
+        }
+        public void UtworzZadaniePrzezFabryke(string tytul,string tresc,Priorytet priorytet,DateTime termin,List<Tag> tagi)
+        {
+            // Wywo³ujemy fabrykê, tworzymy Zadanie
+            var zadanie = (Zadanie)fabryka.UtwórzWpis(tytu³, treœæ, priorytet, termin, tagi);
+
+            // Dodajemy do listy mened¿era
+            zadania.Add(zadanie);
+        }
+        public void UsunZadanie(Zadanie zadanie)
+        {
+            zadania.Remove(zadanie);
+        }
+        public void WypiszZadanie()
+        {
+            foreach (var z in zadania)
+            {
+                Console.WriteLine(z.WypiszInformacje());
+            }
+        }
+        public List<Zadanie> SzukajZadan(string fraza)
+        {
+            List<Zadanie> wynik = new List<Zadanie>();
+
+            foreach (var z in zadania)
+            {
+                // zak³adamy, ¿e Wpis ma dostêp do tytu³u (getter)
+                if (z.WypiszInformacje().Contains(fraza))
+                {
+                    wynik.Add(z);
+                }
+            }
+
+            return wynik;
+        }
+        public void OznaczZadaniaJakoWykonane(List<Zadanie> lista)
+        {
+            foreach (var z in lista)
+            {
+                z.OznaczJakoWykonane();
+            }
+        }
+        public List<Zadanie> WybierzZalegle()
+        {
+            List<Zadanie> zalegle = new List<Zadanie>();
+
+            foreach (var z in zadania)
+            {
+                if (z.SprawdzCzyZalegle())
+                    zalegle.Add(z);
+            }
+
+            return zalegle;
+        }
+    }
+    public class FabrykaZadan : FabrykaWpisow
+    {
+        // Konstruktor
+        public FabrykaZadan() : base()
+        {
+        }
+
+        // Nadpisanie metody fabrykuj¹cej wpis
+        public override Wpis UtworzWpis(string tytul, string tresc, List<Tag> tagi)
+        {
+            // Domyœlne wartoœci priorytetu i terminu, mo¿na póŸniej rozszerzyæ parametry
+            Priorytet domyslnyPriorytet = Priorytet.Normalny;
+            DateTime domyslnyTermin = DateTime.Now.AddDays(7); // np. tydzieñ od dziœ
+
+            return new Zadanie(tytul, tresc, domyslnyPriorytet, domyslnyTermin, tagi);
+        }
+
+       
+    }
+} 
