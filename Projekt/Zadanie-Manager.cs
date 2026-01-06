1de6123ff8c013af138e9b56d1291074851f1e87
Lukasz97947 <91849@student.pb.edu.pl>
2026-01-06 14:21:02 +0100

    Dodanie enumów Stan zadania i Proprytet

---
+++ Projekt/Zadanie-Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+public partial class Program
+{
+
+    public enum StanZadania
+    {
+        Aktywne,
+        Wykonane,
+        Zalegle
+    }
+
+    public enum Priorytet
+    {
+        Niski,
+        Sredni,
+        Wysoki
+    }
+}
